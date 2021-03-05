61ff8326a60692b15574db62d2f8c1b5883587b0
yimy <46787253+yimy1556@users.noreply.github.com>
2021-03-05 12:23:09 -0300

    new deploy

--- src/config/production.tsx
+++ src/config/production.tsx
@@ -1,6 +1,6 @@
 // Place the URL here with the /api suffix.
 // Ex.:`https://domain.com/api`;
-const backendUrl = `http://ns-contro-calidad-backend-ns-ce-dev.pappsn.nwsn.local/api`;
+const backendUrl = `https://newsan-qualitycontrol-test.herokuapp.com/api`;
 
 /**
  * Frontend URL.
